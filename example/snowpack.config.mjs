788488d9318af29f763712fc183618439917b0b2
anze.kolsek <anze.kolsek@gmail.com>
2022-08-10 09:07:28 +0200

    added example and added build, lint etc scripts

---
+++ example/snowpack.config.mjs
@@ -0,0 +1,12 @@
+// Example: snowpack.config.mjs
+// The added "@type" comment will enable TypeScript type information via VSCode, etc.
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+export default {
+  plugins: [
+    /* ... */
+  ],
+//   mount: {
+//     src: 'example/',
+//   },
+};
