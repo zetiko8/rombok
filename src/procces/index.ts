110edacb8c4253e82e3a5d0d68a7666a8f68caf5
anze.kolsek <anze.kolsek@gmail.com>
2023-08-29 23:01:41 +0200

    refactor

--- src/procces/index.ts
+++ src/procces/index.ts
@@ -13,7 +13,6 @@ import {
   observeOn,
   switchMap,
   tap,
-  filter,
   take,
   distinctUntilChanged,
   startWith,
@@ -22,7 +21,10 @@ import {
   mergeMap,
   delay,
 } from 'rxjs/operators';
-import { LoadContext, MULTIPLE_EXECUTIONS_STRATEGY } from '../loading-handling';
+import {
+  LoadContext,
+  MULTIPLE_EXECUTIONS_STRATEGY,
+} from '../loading-handling';
 import { randomString } from '../helpers';
 
 class Pipeline<T> {
@@ -80,245 +82,241 @@ class Pipeline<T> {
 }
 
 export interface IProcess<T> {
-    error$: Observable<Error | null>;
-    inProgress$: Observable<boolean>;
-    success$: Observable<T>;
+  error$: Observable<Error | null>;
+  inProgress$: Observable<boolean>;
+  success$: Observable<T>;
+}
+
+interface UnboundProcess<T> extends IProcess<T> {
+  execute: {
+    (processFunction: () => Observable<T>)
+    : Observable<T>
   }
+}
 
-export class Process<T> implements IProcess<T> {
-    private _loadContext: LoadContext;
-    private _inProgress$
-     = new BehaviorSubject<boolean>(false);
-    public inProgress$: Observable<boolean>;
-    private _error$ = new ReplaySubject<null | Error>(1);
-    public error$ = this._error$
-      .pipe(observeOn(asyncScheduler));
-    private readonly _success$ = new Subject<T>();
-    public readonly success$: Observable<T>;
-
-    private readonly _trigger
-      = new ReplaySubject<() => Observable<T>>(1);
-    private readonly _pipeline$ = new Pipeline<T>();
-
-    private _options: {
-      multipleExecutionsStrategy: MULTIPLE_EXECUTIONS_STRATEGY,
-    };
-
-    constructor (options: {
-      multipleExecutionsStrategy: MULTIPLE_EXECUTIONS_STRATEGY,
-    } = {
-      multipleExecutionsStrategy:
-        MULTIPLE_EXECUTIONS_STRATEGY.CONCURRENT,
-    }) {
-      this._options = options;
-
-      this._loadContext
-        = new LoadContext(this._options.multipleExecutionsStrategy);
-      this.inProgress$ = this._loadContext.isLoading$
+export class ConcurrentProcess<T> implements UnboundProcess<T> {
+    private _loadContext
+      = new LoadContext(MULTIPLE_EXECUTIONS_STRATEGY.CONCURRENT);
+    public inProgress$
+      = this._loadContext.isLoading$
         .pipe(
           startWith(false),
           observeOn(asyncScheduler),
           distinctUntilChanged(),
         );
-
-      if (
-        this._options.multipleExecutionsStrategy
-          ===
-          MULTIPLE_EXECUTIONS_STRATEGY.CONCURRENT
-      ) {
-        this.success$ = this._success$
-          .pipe(
-            observeOn(asyncScheduler),
-            distinctUntilChanged(),
-          );
-      }
-      else if (
-        this._options.multipleExecutionsStrategy
-          ===
-          MULTIPLE_EXECUTIONS_STRATEGY.MERGE_MAP
-      ) {
-        this.success$ = this._success$
-          .pipe(
-            observeOn(asyncScheduler),
-            distinctUntilChanged(),
-          );
-      }
-      else if (
-        this._options.multipleExecutionsStrategy
-          ===
-          MULTIPLE_EXECUTIONS_STRATEGY.SWITCH_MAP
-      ) {
-        this.success$
-          = this._trigger
-            .pipe(
-              switchMap(fn => fn()),
-              observeOn(asyncScheduler),
-              distinctUntilChanged(),
-            );
-      }
-      else throw Error('Not implemented jet');
-    }
+    private _error$ = new ReplaySubject<null | Error>(1);
+    public error$ = this._error$
+      .pipe(observeOn(asyncScheduler));
+    private readonly _success$ = new Subject<T>();
+    public readonly success$
+     = this._success$
+       .pipe(
+         observeOn(asyncScheduler),
+         distinctUntilChanged(),
+       );
+    private readonly _pipeline$ = new Pipeline<T>();
 
     execute (
       processFunction: () => Observable<T>,
     ): Observable<T> {
-      if (
-        this._options.multipleExecutionsStrategy
-        ===
-        MULTIPLE_EXECUTIONS_STRATEGY.CONCURRENT
-      ) {
-        const load$: Observable<T> = (of('immediate')
-          .pipe(
-            observeOn(asyncScheduler),
-            map(() => randomString()),
-            tap((loadToken) => {
-              this._error$.next(null);
-              this._loadContext.registerLoading(loadToken);
-            }),
-            switchMap(
-              (loadToken) => processFunction()
-                .pipe(
-                  take(1),
-                  map(result => {
-                    return {
-                      result,
-                      hasError:
-                    false,
-                      error: null,
-                      loadToken,
-                    };
-                  }),
-                  catchError(error => {
-                    return of({
-                      result: null,
-                      hasError: true,
-                      error: error,
-                      loadToken,
-                    });
-                  }),
-                ),
-            ),
-            mergeMap(resultWrapper => {
-              if (resultWrapper.hasError) {
-                this._loadContext.registerLoadEnd(resultWrapper.loadToken);
-                this._error$.next(resultWrapper.error);
-                return throwError(() => resultWrapper.error);
-              }
-              else return of(resultWrapper);
-            }),
-            tap((resultWrapper) => {
-              this._error$.next(null);
-              this._success$.next(resultWrapper.result as T);
-              this._loadContext.registerLoadEnd(resultWrapper.loadToken);
-            }),
-            map(r => (r.result) as T),
-            take(1),
-            share({ connector: () => new ReplaySubject<T>(1) }),
-          )) as Observable<T>;
-
-        return this._pipeline$.add(load$);
-      }
-      else if (
-        this._options.multipleExecutionsStrategy
-        ===
-        MULTIPLE_EXECUTIONS_STRATEGY.MERGE_MAP
-      ) {
-        return of('immediate')
-          .pipe(
-            tap(() => {
-              this._inProgress$.next(true);
-            }),
-            observeOn(asyncScheduler),
-            tap(() => {
-              this._error$.next(null);
-              this._loadContext.registerLoading('');
-            }),
-            switchMap(
-              () => processFunction()
-                .pipe(take(1)),
-            ),
-            catchError(error => {
-              this._inProgress$.next(false);
-              this._loadContext.registerLoadEnd('');
-              this._error$.next(error);
-              return throwError(error);
-            }),
-            tap((result) => {
-              this._error$.next(null);
-              this._success$.next(result);
-              this._inProgress$.next(false);
-              this._loadContext.registerLoadEnd('');
-            }),
-            take(1),
-          );
-      }
-      else if (
-        this._options.multipleExecutionsStrategy
-        ===
-        MULTIPLE_EXECUTIONS_STRATEGY.SWITCH_MAP
-      ) {
-
-        const load$: Observable<T> = (of('immediate')
-          .pipe(
-            observeOn(asyncScheduler),
-            map(() => randomString()),
-            tap((loadToken) => {
-              this._error$.next(null);
-              this._loadContext.registerLoading(loadToken);
-            }),
-            switchMap(
-              (loadToken) => processFunction()
-                .pipe(
-                  take(1),
-                  map(result => {
-                    return {
-                      result,
-                      hasError:
-                      false,
-                      error: null,
-                      loadToken,
-                    };
-                  }),
-                  catchError(error => {
-                    return of({
-                      result: null,
-                      hasError: true,
-                      error: error,
-                      loadToken,
-                    });
-                  }),
-                ),
-            ),
-            mergeMap(resultWrapper => {
-              if (resultWrapper.hasError) {
-                this._loadContext.registerLoadEnd(resultWrapper.loadToken);
-                this._error$.next(resultWrapper.error);
-                return throwError(() => resultWrapper.error);
-              }
-              else return of(resultWrapper);
-            }),
-            tap((resultWrapper) => {
-              this._error$.next(null);
-              this._success$.next(resultWrapper.result as T);
-              this._loadContext.registerLoadEnd(resultWrapper.loadToken);
-            }),
-            map(r => (r.result) as T),
-            take(1),
-            share({ connector: () => new ReplaySubject<T>(1) }),
-          )) as Observable<T>;
-
-        this._trigger.next(
-          () => {
-            return load$
-              .pipe(
-                catchError(() => EMPTY),
-              );
-          },
+      const load$: Observable<T> = of('immediate')
+        .pipe(
+          observeOn(asyncScheduler),
+          tap(() => {
+            this._error$.next(null);
+            this._loadContext.registerLoading('');
+          }),
+          switchMap(
+            () => processFunction()
+              .pipe(take(1)),
+          ),
+          catchError(error => {
+            this._loadContext.registerLoadEnd('');
+            this._error$.next(error);
+            return throwError(error);
+          }),
+          tap((result) => {
+            this._error$.next(null);
+            this._success$.next(result);
+            this._loadContext.registerLoadEnd('');
+          }),
         );
 
-        return load$;
-      }
-      else throw Error('Not implemented jet');
+      return this._pipeline$.add(load$);
+    }
+}
+export class MergeMapProcess<T> implements UnboundProcess<T> {
+    private _loadContext
+     = new LoadContext(MULTIPLE_EXECUTIONS_STRATEGY.MERGE_MAP);
+    public inProgress$
+     = this._loadContext.isLoading$
+       .pipe(
+         startWith(false),
+         observeOn(asyncScheduler),
+         distinctUntilChanged(),
+       );
+    private _error$ = new ReplaySubject<null | Error>(1);
+    public error$ = this._error$
+      .pipe(observeOn(asyncScheduler));
+    private readonly _success$ = new Subject<T>();
+    public readonly success$
+     = this._success$
+       .pipe(
+         observeOn(asyncScheduler),
+         distinctUntilChanged(),
+       );
+
+    execute (
+      processFunction: () => Observable<T>,
+    ): Observable<T> {
+      return of('immediate')
+        .pipe(
+          observeOn(asyncScheduler),
+          tap(() => {
+            this._error$.next(null);
+            this._loadContext.registerLoading('');
+          }),
+          switchMap(
+            () => processFunction()
+              .pipe(take(1)),
+          ),
+          catchError(error => {
+            this._loadContext.registerLoadEnd('');
+            this._error$.next(error);
+            return throwError(error);
+          }),
+          tap((result) => {
+            this._error$.next(null);
+            this._success$.next(result);
+            this._loadContext.registerLoadEnd('');
+          }),
+        );
+    }
+}
+
+export class SwitchMapProcess<T> implements UnboundProcess<T> {
+  private _loadContext
+   = new LoadContext(MULTIPLE_EXECUTIONS_STRATEGY.SWITCH_MAP);
+  public inProgress$
+  = this._loadContext.isLoading$
+    .pipe(
+      startWith(false),
+      observeOn(asyncScheduler),
+      distinctUntilChanged(),
+    );
+  private _error$ = new ReplaySubject<null | Error>(1);
+  public error$ = this._error$
+    .pipe(observeOn(asyncScheduler));
+
+  private readonly _trigger
+   = new ReplaySubject<() => Observable<T>>(1);
+  private readonly _success$ = new Subject<T>();
+  public readonly success$
+   = this._trigger
+     .pipe(
+       switchMap(fn => fn()),
+       observeOn(asyncScheduler),
+       distinctUntilChanged(),
+     );
+
+  execute (
+    processFunction: () => Observable<T>,
+  ): Observable<T> {
+    const load$: Observable<T> = (of('immediate')
+      .pipe(
+        observeOn(asyncScheduler),
+        map(() => randomString()),
+        tap((loadToken) => {
+          this._error$.next(null);
+          this._loadContext.registerLoading(loadToken);
+        }),
+        switchMap(
+          (loadToken) => processFunction()
+            .pipe(
+              take(1),
+              map(result => {
+                return {
+                  result,
+                  hasError:
+                false,
+                  error: null,
+                  loadToken,
+                };
+              }),
+              catchError(error => {
+                return of({
+                  result: null,
+                  hasError: true,
+                  error: error,
+                  loadToken,
+                });
+              }),
+            ),
+        ),
+        mergeMap(resultWrapper => {
+          if (resultWrapper.hasError) {
+            this._loadContext.registerLoadEnd(resultWrapper.loadToken);
+            this._error$.next(resultWrapper.error);
+            return throwError(() => resultWrapper.error);
+          }
+          else return of(resultWrapper);
+        }),
+        tap((resultWrapper) => {
+          this._error$.next(null);
+          this._success$.next(resultWrapper.result as T);
+          this._loadContext.registerLoadEnd(resultWrapper.loadToken);
+        }),
+        map(r => (r.result) as T),
+        share({ connector: () => new ReplaySubject<T>(1) }),
+      )) as Observable<T>;
+
+    this._trigger.next(
+      () => {
+        return load$
+          .pipe(
+            catchError(() => EMPTY),
+          );
+      },
+    );
+
+    return load$;
+  }
+}
+
+export class Process<ReturnType> implements IProcess<ReturnType> {
+
+    private _process: UnboundProcess<ReturnType>;
+    error$: Observable<Error | null>;
+    inProgress$: Observable<boolean>;
+    success$: Observable<ReturnType>;
+    execute: {
+      (processFunction: () => Observable<ReturnType>)
+      : Observable<ReturnType>
+    }
+
+    constructor (
+      options: {
+        multipleExecutionsStrategy:
+          MULTIPLE_EXECUTIONS_STRATEGY
+      } = {
+        multipleExecutionsStrategy:
+          MULTIPLE_EXECUTIONS_STRATEGY.MERGE_MAP,
+      },
+    ) {
+      this._process = (() => {
+        switch (options.multipleExecutionsStrategy) {
+        case MULTIPLE_EXECUTIONS_STRATEGY.CONCURRENT:
+          return new ConcurrentProcess();
+        case MULTIPLE_EXECUTIONS_STRATEGY.SWITCH_MAP:
+          return new SwitchMapProcess();
+        default:
+          return new MergeMapProcess();
+        }
+      })();
+      this.error$ = this._process.error$;
+      this.inProgress$ = this._process.inProgress$;
+      this.success$ = this._process.success$;
+      this.execute = this._process.execute.bind(this._process);
     }
 }
 
