d3c5a466f0c6c02ffc0eda50364bc54486e57ae4
anze.kolsek <anze.kolsek@gmail.com>
2023-08-30 11:53:34 +0200

    added some tests

---
+++ test/process/use-cases/first-and-second-fire-at-same-time-on-the-start-and-take-no-time.spec.ts
@@ -0,0 +1,113 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Process } from '../../../src';
+import { MULTIPLE_EXECUTIONS_STRATEGY } from '../../../src/loading-handling';
+import { TestScheduler } from 'rxjs/testing';
+import { createSandbox, SinonSandbox } from 'sinon';
+import { assertCallCount, ColdCreator, ignoreErrorSub, prepareTestScheduler, spy, TestError, TestScenarioReturn, values } from '../../test.helpers';
+import { map } from 'rxjs';
+
+/**
+ * Check what happens if everything happens
+ * instantly - make sure that the interfaces are in correct state
+ * // TODO - also test for errors
+ */
+describe.only('first and second fire at same time on the start and take no time', () => {
+  let scheduler: TestScheduler;
+  let sbx: SinonSandbox;
+  beforeEach(() => {
+    scheduler = prepareTestScheduler();
+    sbx = createSandbox();
+  });
+  afterEach(() => {
+    scheduler = prepareTestScheduler();
+    sbx.restore();
+  });
+
+  const scenario = (
+    process: Process<string>,
+    cold: ColdCreator,
+  ): TestScenarioReturn => {
+
+    const error = new TestError('test');
+    const spyWrapper = spy(sbx, (value: string) => {
+      return cold(value);
+    });
+    function onWrite (value: string) {
+      process.execute(
+        () => spyWrapper.fn(value))
+        .subscribe(ignoreErrorSub);
+    }
+
+    // user writes
+    cold('o').subscribe(onWrite);
+    cold('p').subscribe(onWrite);
+    cold('-----------r').subscribe(onWrite);
+
+    const after
+    = cold('-------------------------1')
+      .pipe(map(() => undefined));
+
+    return [ error, spyWrapper.spy, after ];
+  };
+
+  it('merge', () => {
+    scheduler.run(({ cold, expectObservable }) => {
+      const process
+       = new Process<string>({
+         multipleExecutionsStrategy:
+          MULTIPLE_EXECUTIONS_STRATEGY.MERGE_MAP,
+       });
+      const [ error, processFn, after ]
+        = scenario(process, cold);
+
+      expectObservable(process.success$)
+        .toBe('(op)----------r');
+      expectObservable(process.error$)
+        .toBe('n------------', { ...values, e: error });
+      expectObservable(process.inProgress$)
+        .toBe('(ftf)----------(tf)', values);
+      after.subscribe(() =>
+        assertCallCount(processFn, 3));
+    });
+  });
+  it('concat', () => {
+    scheduler.run(({ cold, expectObservable }) => {
+      const process
+       = new Process<string>({
+         multipleExecutionsStrategy:
+          MULTIPLE_EXECUTIONS_STRATEGY.CONCAT_MAP,
+       });
+      const [ error, processFn, after ]
+        = scenario(process, cold);
+
+      expectObservable(process.success$)
+        .toBe('op---------r');
+      expectObservable(process.error$)
+        .toBe('n------------', { ...values, e: error });
+      expectObservable(process.inProgress$)
+        .toBe('(ftf)(tf)---------(tf)', values);
+      after.subscribe(() =>
+        assertCallCount(processFn, 3));
+    });
+  });
+  it.only('switch', () => {
+    scheduler.run(({ cold, expectObservable }) => {
+      const process
+       = new Process<string>({
+         multipleExecutionsStrategy:
+          MULTIPLE_EXECUTIONS_STRATEGY.SWITCH_MAP,
+       });
+      const [ error, processFn, after ]
+        = scenario(process, cold);
+
+      expectObservable(process.success$)
+        .toBe('p----------r');
+      expectObservable(process.error$)
+        .toBe('n------------', { ...values, e: error });
+      expectObservable(process.inProgress$)
+        .toBe('(ftf)----------(tf)', values);
+      after.subscribe(() =>
+        assertCallCount(processFn, 3));
+    });
+  });
+});
---
+++ test/process/use-cases/first-and-second-fire-at-same-time.spec.ts
@@ -0,0 +1,114 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Process } from '../../../src';
+import { MULTIPLE_EXECUTIONS_STRATEGY } from '../../../src/loading-handling';
+import { TestScheduler } from 'rxjs/testing';
+import { createSandbox, SinonSandbox } from 'sinon';
+import { assertCallCount, ColdCreator, ignoreErrorSub, prepareTestScheduler, spy, TestError, TestScenarioReturn, values } from '../../test.helpers';
+import { map } from 'rxjs';
+
+/**
+ * Test if the loading indicator fires correctly
+ * meaning that a finished request does not set the
+ * loading to false, despite another request still
+ * running
+ */
+describe('first and second fire at same time', () => {
+  let scheduler: TestScheduler;
+  let sbx: SinonSandbox;
+  beforeEach(() => {
+    scheduler = prepareTestScheduler();
+    sbx = createSandbox();
+  });
+  afterEach(() => {
+    scheduler = prepareTestScheduler();
+    sbx.restore();
+  });
+
+  const scenario = (
+    process: Process<string>,
+    cold: ColdCreator,
+  ): TestScenarioReturn => {
+
+    const error = new TestError('test');
+    const spyWrapper = spy(sbx, (value: string) => {
+      return cold('-' + value);
+    });
+    function onWrite (value: string) {
+      process.execute(
+        () => spyWrapper.fn(value))
+        .subscribe(ignoreErrorSub);
+    }
+
+    // user writes
+    cold('---o').subscribe(onWrite);
+    cold('---p').subscribe(onWrite);
+    cold('-----------r').subscribe(onWrite);
+
+    const after
+    = cold('-------------------------1')
+      .pipe(map(() => undefined));
+
+    return [ error, spyWrapper.spy, after ];
+  };
+
+  it('merge', () => {
+    scheduler.run(({ cold, expectObservable }) => {
+      const process
+       = new Process<string>({
+         multipleExecutionsStrategy:
+          MULTIPLE_EXECUTIONS_STRATEGY.MERGE_MAP,
+       });
+      const [ error, processFn, after ]
+        = scenario(process, cold);
+
+      expectObservable(process.success$)
+        .toBe('----(op)-------r');
+      expectObservable(process.error$)
+        .toBe('n------------', { ...values, e: error });
+      expectObservable(process.inProgress$)
+        .toBe('f--tf------tf', values);
+      after.subscribe(() =>
+        assertCallCount(processFn, 3));
+    });
+  });
+  it('concat', () => {
+    scheduler.run(({ cold, expectObservable }) => {
+      const process
+       = new Process<string>({
+         multipleExecutionsStrategy:
+          MULTIPLE_EXECUTIONS_STRATEGY.CONCAT_MAP,
+       });
+      const [ error, processFn, after ]
+        = scenario(process, cold);
+
+      expectObservable(process.success$)
+        .toBe('----o-p-----r');
+      expectObservable(process.error$)
+        .toBe('n------------', { ...values, e: error });
+      expectObservable(process.inProgress$)
+        .toBe('f--tftf----tf', values);
+      after.subscribe(() =>
+        assertCallCount(processFn, 3));
+    });
+  });
+  it('switch', () => {
+    scheduler.run(({ cold, expectObservable }) => {
+      const process
+       = new Process<string>({
+         multipleExecutionsStrategy:
+          MULTIPLE_EXECUTIONS_STRATEGY.SWITCH_MAP,
+       });
+      const [ error, processFn, after ]
+        = scenario(process, cold);
+
+      expectObservable(process.success$)
+        .toBe('----p-------r');
+      expectObservable(process.error$)
+        .toBe('n------------', { ...values, e: error });
+      expectObservable(process.inProgress$)
+        .toBe('f--tf------tf', values);
+      after.subscribe(() =>
+        assertCallCount(processFn, 3));
+    });
+  });
+});
