96e04a7086828b5cd04e92712e80cfd076433bd1
anze.kolsek <anze.kolsek@gmail.com>
2023-10-17 12:28:03 +0200

    added errorFirstToResult and errorFirstToError

--- src/error-first.ts
+++ src/error-first.ts
@@ -1,5 +1,5 @@
-import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { EMPTY, Observable, of } from 'rxjs';
+import { catchError, map, mergeMap } from 'rxjs/operators';
 
 export type ErrorFirst<T> = [
   error: null,
@@ -18,6 +18,32 @@ export const errorFirst
     catchError(error => {
       const ef: ErrorFirst<T> = [ error, null ];
       return of(ef);
+    }),
+  );
+};
+
+export const errorFirstToResult
+= <T>(): (source$: Observable<ErrorFirst<T>>)
+  => Observable<T> => {
+  return (
+    source$: Observable<ErrorFirst<T>>,
+  ): Observable<T> => source$.pipe(
+    mergeMap(([error, result]) => {
+      if (error) return EMPTY;
+      return of(result as T);
+    }),
+  );
+};
+
+export const errorFirstToError
+= <T>(): (source$: Observable<ErrorFirst<T>>)
+  => Observable<Error | null> => {
+  return (
+    source$: Observable<ErrorFirst<T>>,
+  ): Observable<Error | null> => source$.pipe(
+    mergeMap(([error]) => {
+      if (error === null) return EMPTY;
+      return of(error);
     }),
   );
 };
